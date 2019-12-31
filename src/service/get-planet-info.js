16a6c76b0115570b57e124720296323fd2289a1e
salmanthecoder <rahilsalman@gmail.com>
2019-12-31 23:30:50 +0100

    added docker compose yml

--- src/service/get-planet-info.js
+++ src/service/get-planet-info.js
@@ -1,5 +1,5 @@
 export async function getPlanetInfo () {
-    return await fetch(`https://planetinfo.herokuapp.com/planet/info`)
+    return await fetch(`http://planetinfo.herokuapp.com/planet/info`)
       .then(r => r.json())
       .then( data => {
         return data;
