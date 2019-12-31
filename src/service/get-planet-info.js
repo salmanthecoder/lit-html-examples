83625dc0ee2bb0aa939be953b1cf69d3d240c61f
salmanthecoder <rahilsalman@gmail.com>
2019-12-31 22:14:54 +0100

    updated get service url

--- src/service/get-planet-info.js
+++ src/service/get-planet-info.js
@@ -1,5 +1,5 @@
 export async function getPlanetInfo () {
-    return await fetch(`http://localhost:8080/planet/info`)
+    return await fetch(`https://planetinfo.herokuapp.com/planet/info`)
       .then(r => r.json())
       .then( data => {
         return data;
